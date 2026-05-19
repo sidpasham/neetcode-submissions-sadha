d57570fac1ed45e593ffaef4af504fb8b6d32f7e
Siddhanth Pasham <44481936+sidpasham@users.noreply.github.com>
2026-05-19 08:27:14 -0500

    Add: k-closest-points-to-origin - submission-1

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-1.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int[][] KClosest(int[][] points, int k) {
+        var pq = new PriorityQueue<int[], int>();
+
+        foreach(var point in points) {
+            int dist = point[0] * point[0] + point[1] * point[1];
+            pq.Enqueue(new int[] { point[0], point[1] }, dist);
+        }
+
+        int[][] result = new int[k][];
+        for (int i = 0; i < k; ++i) {
+            int[] point = pq.Dequeue();
+            result[i] = new int[] { point[0], point[1] };
+        }
+        return result;      
+    }
+}
