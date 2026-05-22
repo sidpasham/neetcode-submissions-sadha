428b605f08a79c1699b56c327a7e2ca8c32509a9
Siddhanth Pasham <44481936+sidpasham@users.noreply.github.com>
2026-05-21 20:03:42 -0500

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public List<List<int>> Subsets(int[] nums) {
+        var res = new List<List<int>>();
+        var subset = new List<int>();
+        dfs(nums, subset, 0, res);
+        return res;
+    }
+
+    public void dfs(int[] nums, List<int> subset, int i, List<List<int>> res) {
+        if(i >= nums.Length) {
+            var copy = new List<int>(subset);
+            res.Add(copy);
+            return;
+        }
+
+        subset.Add(nums[i]);
+        dfs(nums, subset, i+1, res);
+
+        // remove the elements while tracking back as slowly move up
+        subset.Remove(subset[subset.Count - 1]);
+        dfs(nums, subset, i+1, res);
+    }
+}
