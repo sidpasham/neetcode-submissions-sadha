65665467564b560ccfc35c0721192c90d9fb41af
Siddhanth Pasham <44481936+sidpasham@users.noreply.github.com>
2026-05-21 20:42:38 -0500

    Add: combination-target-sum - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum/submission-0.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public List<List<int>> CombinationSum(int[] nums, int target) {
+        var res = new List<List<int>>();
+        var subset = new List<int>();
+        dfs(nums, target, 0, 0, subset, res);
+        return res;
+    }
+
+    public void dfs(int[] nums, int target, int i, int sum, List<int> subset, List<List<int>> res) {
+        if(sum == target) {
+            res.Add(new List<int>(subset));
+            return;
+        }
+
+        if(i >= nums.Length || sum > target) {
+            return;
+        }
+
+        subset.Add(nums[i]);
+        dfs(nums, target, i, sum + nums[i], subset, res);
+
+        subset.Remove(nums[i]);
+        dfs(nums, target, i+1, sum, subset, res);
+    }
+}
