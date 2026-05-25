45e6cace06c74d3e790f169f96925b603be8ff32
Siddhanth Pasham <44481936+sidpasham@users.noreply.github.com>
2026-05-25 11:44:38 -0500

    Add: kth-largest-element-in-an-array - submission-1

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-1.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int FindKthLargest(int[] nums, int k) {
+        var pq = new PriorityQueue<int, int>();
+        foreach(var num in nums) {
+            pq.Enqueue(num, num);
+            if(pq.Count > k) {
+                pq.Dequeue();
+            }
+        }
+        return pq.Peek();
+    }
+}
