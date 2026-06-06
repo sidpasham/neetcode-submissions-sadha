43f9458c433ccd8d3f77d366455571a16a38875c
Siddhanth Pasham <44481936+sidpasham@users.noreply.github.com>
2026-06-06 17:12:36 -0500

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public int Rob(int[] nums) {
+        // base case
+        if(nums == null || nums.Length == 0) {
+            return 0;
+        }
+
+        int n = nums.Length;
+
+        if(n == 1) {
+            return nums[0];
+        }
+
+        int max = 0;
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.Max(nums[0], nums[1]);
+
+
+        for(int i = 2; i<n; i++) {
+            dp[i] = Math.Max(dp[i-1], nums[i] + dp[i-2]);
+        }
+        
+        return dp[n-1];
+    }
+}
