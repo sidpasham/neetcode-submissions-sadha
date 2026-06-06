b4e000dddccd2288cd959b2830e64e0f51369dab
Siddhanth Pasham <44481936+sidpasham@users.noreply.github.com>
2026-06-06 16:54:28 -0500

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int ClimbStairs(int n) {
+        // base case where 1 = 1 and 2 = 2 (1,1)    
+        if (n <= 2) {
+            return n;
+        }
+
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        } 
+
+        return dp[n];
+    }
+}
