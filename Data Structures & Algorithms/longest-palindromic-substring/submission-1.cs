5a584b8dd33de2019eab757b032f2c1d52fa9f78
Siddhanth Pasham <44481936+sidpasham@users.noreply.github.com>
2026-06-07 08:42:43 -0500

    Add: longest-palindromic-substring - submission-1

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-1.cs
@@ -0,0 +1,39 @@
+public class Solution {
+    public string LongestPalindrome(string s) {
+        // edge case
+        if(s == null || s.Length == 0) {
+            return s;
+        }
+
+        int resIndex = 0;
+        int resLength = 0;
+
+        for(int i = 0; i < s.Length; i++) {
+            // odd length
+            int l = i;
+            int r = i;
+            while(l >=0 && r < s.Length && s[l] == s[r]) {
+                if(r - l + 1 > resLength) {
+                    resIndex = l;
+                    resLength = r - l + 1;
+                }
+                l--;
+                r++;
+            }
+
+            // even Length
+            l = i;
+            r = i+1;
+            while(l >=0 && r < s.Length && s[l] == s[r]) {
+                if(r - l + 1 > resLength) {
+                    resIndex = l;
+                    resLength = r - l + 1;
+                }                
+                l--;
+                r++;
+            }
+        }
+
+        return s.Substring(resIndex, resLength);
+    }
+}
