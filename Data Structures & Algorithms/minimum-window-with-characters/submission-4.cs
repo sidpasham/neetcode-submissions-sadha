3d349de0af47522ad76eff8c55e0074a28a20132
Sadha Shiva Siddhanth Pasham <44481936+sidpasham@users.noreply.github.com>
2026-06-29 21:36:44 -0500

    Add: minimum-window-with-characters - submission-4

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-4.cs
@@ -0,0 +1,77 @@
+public class Solution {
+    public string MinWindow(string s, string t) {
+        // Edge cases
+        if (s == null || t == null || s.Length < t.Length || t.Length == 0) {
+            return "";
+        }
+
+        Dictionary<char, int> countT = new Dictionary<char, int>();
+        Dictionary<char, int> window = new Dictionary<char, int>();
+
+        // count the chars of c
+        foreach(var c in t) {
+            if(countT.ContainsKey(c)) {
+                countT[c]++;
+            } else {
+                countT[c] = 1;
+            }            
+        }
+
+
+        int have = 0;
+        int need = countT.Count;
+
+        int l = 0;
+        int r = 0;
+
+        int min = int.MaxValue;
+        int[] result = new int[2];
+
+        while(r < s.Length) {
+            var c = s[r];
+            
+            // count window chars
+            if (window.ContainsKey(c)) {
+                window[c]++;
+            } else {
+                window[c] = 1;
+            }
+
+            // it counts are equal then increment have
+            if(countT.ContainsKey(c) && window.ContainsKey(c)
+             && countT[c] == window[c]) {                
+                have++;
+            }
+
+            // if have == need, cut the left window by removing chars and decrementing the counts
+            while(have == need) {
+                var curLen = r - l + 1;
+
+                // shortest length found, store result
+                if(curLen < min) {
+                    result[0] = l;
+                    result[1] = r;
+                    min = curLen;
+                }
+
+                // remove the left char to move window
+                var leftChar = s[l];
+                window[leftChar]--;
+
+                // if left char does not reach the satisfying condition
+                if(countT.ContainsKey(leftChar) && window[leftChar] < countT[leftChar]) {
+                    have--;
+                }
+
+                // move the left window
+                l++;                
+            }
+
+            // move the right window
+            r++;            
+        }
+
+        // if never found, return empty, otherwise the substring
+        return min == int.MaxValue ? "" : s.Substring(result[0], min);
+    }
+}
