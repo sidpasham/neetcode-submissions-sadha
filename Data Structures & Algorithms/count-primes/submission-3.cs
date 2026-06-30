dbdc8da49c6133d4a04ad6eb31b88281f46bf0cf
Sadha Shiva Siddhanth Pasham <44481936+sidpasham@users.noreply.github.com>
2026-06-29 21:55:02 -0500

    Add: count-primes - submission-3

---
+++ Data Structures & Algorithms/count-primes/submission-3.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public int CountPrimes(int n) {
+        // There are no primes strictly less than 2
+        if (n <= 2) return 0;
+
+        // isComposite[i] will be true if 'i' is NOT a prime number.
+        // Default values in a bool array are false.
+        bool[] isComposite = new bool[n];
+        int count = 0;
+
+        // Loop up to the square root of n
+        for (int num = 2; num < n; num++) {
+            if (!isComposite[num]) {
+                count++;
+                // Mark all multiples of num starting from num * num as composite
+                for (long i = (long)num * num; i < n; i += num) {
+                    isComposite[i] = true;
+                }
+            }
+        }
+
+        return count;
+    }
+}
