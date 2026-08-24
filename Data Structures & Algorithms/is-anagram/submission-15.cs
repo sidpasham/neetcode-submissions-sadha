5608898d41e2fbe57c0d4a6dbfbfee1135afb3ef
Sadha Shiva Siddhanth Pasham <44481936+sidpasham@users.noreply.github.com>
2026-08-24 13:46:38 -0500

    Add: is-anagram - submission-15

---
+++ Data Structures & Algorithms/is-anagram/submission-15.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        if(s == null || t == null) {
+            return s == t;
+        }
+
+        // edge cases
+        if (s.Length != t.Length) {
+            return false;
+        }
+
+        int[] s_count = new int[256];
+        int[] t_count = new int[256];
+
+        foreach(var c in s) {
+            s_count[c]++;
+        }
+
+        foreach(var c in t) {
+            t_count[c]++;
+        }
+
+        foreach(var c in s) {
+            if(t_count[c] != s_count[c]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
