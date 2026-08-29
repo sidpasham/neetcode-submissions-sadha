d00f85dcd6cfc83187655465737960ff75cd9fa0
Sadha Shiva Siddhanth Pasham <44481936+sidpasham@users.noreply.github.com>
2026-08-29 09:34:00 -0500

    Add: two-integer-sum-ii - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-3.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public int[] TwoSum(int[] numbers, int target) {
+        if (numbers == null) {
+            return new int[] {-1, -1};
+        }
+
+        int l = 0;
+        int r = numbers.Length - 1;
+
+        while (l < r) {
+            int sum = numbers[l] + numbers[r];
+
+            if(sum < target) {
+                l++;
+            } else if (sum > target) {
+                r--;
+            } else if (sum == target) {
+                return new int[] {l + 1, r + 1};
+            }
+        }
+
+        return new int[] {-1, -1};
+    }
+}
